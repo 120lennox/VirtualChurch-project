3cdc49d4c87b235908225072ef5087f99f05cba9
Lenoil3rd <bed-com-37-21@unima.ac.mw>
2024-05-08 08:41:06 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function App() {
+  return <div className="flex flex-row w-full h-screen items-center justify-center bg-blue-600">Suiiii!!!!</div>
+    
+}
+
+export default App;
