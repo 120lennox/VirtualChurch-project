444279e90dc02212c43bd0148d4844be21ebfa13
Lenoil3rd <bed-com-37-21@unima.ac.mw>
2024-05-26 20:41:37 +0200

    updated appearance and adjusted for responsiveness on smaller devices

--- virtual-church/src/nav-elements/Login.jsx
+++ virtual-church/src/nav-elements/Login.jsx
@@ -1,37 +1,42 @@
 import { FcGoogle } from "react-icons/fc"
+
+
 export default function Login(){
     return (
-        <div className="flex justify-center items-center">
-            <div className="bg-cyan-900 rounded-md p-8 mt-20 mb-10">
+        <div className="flex justify-center items-center bg-cyan-950 h-full ">
+            <div className="bg-cyan-900 rounded-lg text-cyan-50 flex flex-col justify-center items-center p-12  mb-40 mt-20">
                 <div className="flex flex-col justify-center items-center">
                     <div className="mb-5">
-                        <h1 className="font-bold text-3xl sm:text-4xl">Signin</h1>
+                        <h1 className="font-bold text-3xl sm:text-4xl text-gray-300 ">Signin</h1>
                     </div>
-                    <div className="space-x-4 mt-8">
-                        <label htmlFor="username">Username:</label>
-                        <input className="py-1 px-7 sm:px-12 rounded-md focus:ring-cyan-600 focus:ring-transparent" type="text" id="username" placeholder="e.g John Doe" />
+                    <div className=" space-x-2 sm:space-x-4 mt-8">
+                    
+                        <label className="text-gray-300 font-semibold" htmlFor="username">Username</label>
+                        <input className="py-1 px-1 w-36 sm:w-60  rounded-md outline-none  focus:ring-2 focus:ring-yellow-400 bg-slate-200 " type="text" id="username" placeholder="e.g Lenoil Amulus" />
                     </div>
-
-                    <div className="space-x-4 mt-8">
-                        <label htmlFor="password">Password:</label>
-                        <input className="py-1 px-7 sm:px-12 rounded-md focus:ring-cyan-600 focus:ring-transparent" type="text" id="password" />
+                    
+                    <div className="space-x-2 sm:space-x-5 mt-8">
+                    
+                        <label className="text-gray-300 font-semibold" htmlFor="password">Password</label>
+                        <input className="py-1 px-1 w-36 sm:w-60  rounded-md outline-none  focus:ring-2 focus:ring-yellow-400 bg-slate-200" type="password" id="password" />
                     </div>
-                    <div className="mt-8 flex flex-row space-x-1 text-cyan-100">
+                    <div className="mt-8 flex flex-row space-x-1 text-cyan-100">                     
                         <input className="rounded-md" type="checkbox" id="remember-me" />
                         <label htmlFor="remember-me">remember me </label>
                     </div>
-                    <div className="mt-8 bg-yellow-400 rounded-md py-1 px-8 cursor-pointer hover:bg-yellow-700 hover:text-white duration-150 ease-in-out">
-                        <input className="cursor-pointer" type="submit" value="Signin" />
+                    <div>
+                        <button className="flex flex-row items-center bg-cyan-600 rounded-md mt-10  py-2 px-10 mb-5 hover:scale-90 duration-200 ease-in-out" type="submit" ><p className="font-semibold text-cyan-50" >SignIn</p></button>
+                        
                     </div>
                     
-                    <div className="mt-8 ">
-                        <p>or</p>
+                    <div className="mt-5 mb-5 ">
+                        <p className="text-gray-300 font-semibold">or</p>
                     </div>
-                    <div className="mt-8 mb-12">
-                        <button className="flex flex-row items-center space-x-1 bg-cyan-600 py-2 px-4 rounded-md hover:scale-110 duration-200 ease-in-out">
-                            <FcGoogle />
-                            <p>Continue with google</p>
-                        </button>
+                    <div>
+                    <button className="flex flex-row items-center border-yellow-400 border-2 bg-transparent outline-4 rounded-md py-2 px-10  hover:scale-90 duration-200 ease-in-out">
+                      <p className="text-gray-400 font-semibold ">Continue with</p>
+                      <FcGoogle className="inline-block" />
+                      </button>
                     </div>
                 </div>
             </div>
